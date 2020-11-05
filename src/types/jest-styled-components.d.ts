26d80a3b1ce8501822503a25bb2a8ae853c4b56b
bsignx <bsignx@gmail.com>
2020-11-05 10:03:03 -0300

    feat: add more options heading component

--- src/types/jest-styled-components.d.ts
+++ src/types/jest-styled-components.d.ts
@@ -1,6 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 // Types provided from the official repo:
 // https://github.com/styled-components/jest-styled-components/blob/master/typings/index.d.ts
-
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/ban-types */
 import { Plugin, NewPlugin } from 'pretty-format'
@@ -20,7 +20,6 @@ declare global {
       supports?: string
     }
 
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     interface Matchers<R, T> {
       toHaveStyleRule(property: string, value?: Value, options?: Options): R
     }
