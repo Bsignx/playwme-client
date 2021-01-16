b8bc192b5d48ad0abf3292f9e4e148053d223e7f
bruno.marianocd <bruno.mariano>
2021-01-16 17:09:26 -0300

    feat: add  Game page

--- src/components/GameDetails/index.tsx
+++ src/components/GameDetails/index.tsx
@@ -12,6 +12,7 @@ type Rating = 'BR0' | 'BR10' | 'BR12' | 'BR14' | 'BR16' | 'BR18'
 export type GameDetailsProps = {
   developer: string
   platforms: Platform[]
+  publisher: string
   releaseDate: string
   rating: Rating
   genres: string[]
@@ -20,6 +21,7 @@ export type GameDetailsProps = {
 const GameDetails = ({
   developer,
   releaseDate,
+  publisher,
   platforms,
   rating,
   genres
@@ -66,7 +68,7 @@ const GameDetails = ({
 
         <S.Block>
           <S.Label>Publisher</S.Label>
-          <S.Description>2K</S.Description>
+          <S.Description>{publisher}</S.Description>
         </S.Block>
 
         <S.Block>
