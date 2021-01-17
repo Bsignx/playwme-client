3407b1d074bdaf1e38238755ca8c991834e0d3b1
bruno.marianocd <bruno.mariano>
2021-01-17 10:04:25 -0300

    feat: add games to Wishlist Page

---
+++ src/templates/Wishlist/index.tsx
@@ -0,0 +1,38 @@
+import Base from 'templates/Base'
+import { Container } from 'components/Container'
+import Heading from 'components/Heading'
+import Showcase from 'components/Showcase'
+import GameCard, { GameCardProps } from 'components/GameCard'
+import { HighlightProps } from 'components/Highlight'
+
+export type WishlistTemplateProps = {
+  recommendedGames: GameCardProps[]
+  recommendedHighlight: HighlightProps
+  games?: GameCardProps[]
+}
+
+const Wishlist = ({
+  games,
+  recommendedGames,
+  recommendedHighlight
+}: WishlistTemplateProps) => (
+  <Base>
+    <Container>
+      <Heading lineLeft lineColor="secondary">
+        Wishlist
+      </Heading>
+
+      {games?.map((game, index) => (
+        <GameCard key={`wishlist-${index}`} {...game} />
+      ))}
+    </Container>
+
+    <Showcase
+      title="You may like these games"
+      games={recommendedGames}
+      highlight={recommendedHighlight}
+    />
+  </Base>
+)
+
+export default Wishlist
