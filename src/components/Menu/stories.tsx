ed6455e6efc1edc906fde6ce0390673bc238b8c8
bruno.marianocd <bruno.mariano>
2021-02-06 16:53:33 -0300

    feat: adjust Menu to have correct items and dropdown

--- src/components/Menu/stories.tsx
+++ src/components/Menu/stories.tsx
@@ -4,7 +4,13 @@ import { MenuProps } from '.'
 
 export default {
   title: 'Menu',
-  component: Menu
+  component: Menu,
+  parameters: {
+    layout: 'fullscreen',
+    backgrounds: {
+      default: 'won-dark'
+    }
+  }
 } as Meta
 
 export const Default: Story<MenuProps> = (args) => <Menu {...args} />
@@ -12,4 +18,10 @@ export const Default: Story<MenuProps> = (args) => <Menu {...args} />
 Default.parameters = {
   layout: 'fullscreen',
   backgrounds: { default: 'won-dark' }
+}
+
+export const Logged: Story<MenuProps> = (args) => <Menu {...args} />
+
+Logged.args = {
+  username: 'John Doe'
 }
